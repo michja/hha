efefe30ec3bba64bb58f90b72f637a0aa1380618
Thorsten Lorenz <thlorenz@gmx.de>
2016-03-03 12:36:23 -0500

    fixing collect and positions

--- hha.js
+++ hha.js
@@ -7,3 +7,25 @@ module.exports = function analyze (hand) {
   if (!hand.info) throw new Error('Hand is missing info')
   if (hand.info.pokertype === 'holdem') return analyzeHoldem(hand)
 }
+
+// Test
+function inspect (obj, depth) {
+  console.error(require('util').inspect(obj, false, depth || 5, true))
+}
+if (!module.parent && typeof window === 'undefined') {
+  const fs = require('fs')
+  const path = require('path')
+  const hhv_fixtures = path.join(__dirname, '..', 'hhv', 'test', 'fixtures', 'holdem')
+
+  // const name = 'actiononall'
+  const name = 'allin-preflop'
+
+  const hand = require('./test/fixtures/holdem/' + name + '.json')
+  const analyzed = module.exports(hand)
+
+  inspect(analyzed)
+
+  fs.writeFileSync(path.join(hhv_fixtures, name + '.json'),
+                   JSON.stringify(analyzed, null, 2),
+                   'utf8')
+}
--- test/holdem.js
+++ test/holdem.js
@@ -10,6 +10,14 @@ const ocat = require('./util/ocat')
 function inspect (obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true))
 }
+function diagnosePositions (players) {
+  players.forEach(diagnosePosition)
+  function diagnosePosition (p) {
+    const pos = p.sb ? 'sb' : p.bb ? 'bb' : p.bu ? 'bu' : 'na'
+    console.log('[ seat: %d, [ %s ], pref: %d, post: %d, %s ] \t(%s)',
+                p.seatno, pos, p.preflopOrder, p.postflopOrder, p.pos, p.name)
+  }
+}
 /* eslint-ensable no-unused-vars */
 
 test('\naction on all streets', function (t) {
@@ -59,6 +67,7 @@ test('\naction on all streets', function (t) {
     , flop: []
     , turn: []
     , river: []
+    , showdown: []
     , sb: true
     , preflopOrder: 2
     , postflopOrder: 0
@@ -78,6 +87,7 @@ test('\naction on all streets', function (t) {
     , flop: []
     , turn: []
     , river: []
+    , showdown: []
     , hero: true
     , cards: { card1: '4c', card2: '2d' }
     , bb: true
@@ -114,6 +124,7 @@ test('\naction on all streets', function (t) {
         , allin: false
         , amount: 3200
         , pot: 15800 } ]
+    , showdown: []
     , preflopOrder: 0
     , postflopOrder: 2
     , pos: 'co'
@@ -136,13 +147,12 @@ test('\naction on all streets', function (t) {
     , flop: [ { type: 'call', ratio: 0.3, allin: false, amount: 2400, pot: 7000 } ]
     , turn: [ { type: 'bet', ratio: 0.2, allin: false, amount: 1600, pot: 9400 } ]
     , river: [ { type: 'bet', ratio: 0.3, allin: false, amount: 3200, pot: 12600 } ]
+    , showdown: [ { type: 'collect', ratio: 1, winall: true, amount: 19000 } ]
     , button: true
     , preflopOrder: 1
     , postflopOrder: 3
     , pos: 'bu'
     , cards: { card1: 'Jh', card2: 'Qs' }
-    , collect: 1
-    , winall: true
     , name: 'Irisha2'
     , invested: true })
 
@@ -150,7 +160,7 @@ test('\naction on all streets', function (t) {
 })
 
 test('\npreflop allin', function (t) {
-  t.pass('original HH for reference: https://github.com/thlorenz/hhp/blob/7ab748013ff7b2f762497abbd55f04d25d387701/test/fixtures/holdem/pokerstars/allin-preflop.txt')
+  t.pass('original HH for reference: https://github.com/thlorenz/hhp/blob/master/test/fixtures/holdem/pokerstars/allin-preflop.txt')
   const hand = require('./fixtures/holdem/allin-preflop.json')
   const res = analyze(hand)
 
@@ -185,7 +195,102 @@ test('\npreflop allin', function (t) {
     { $topic: 'board', card1: '8h', card2: 'Kd', card3: '2s', card4: '6s', card5: '4s' })
 
   t.equal(res.players.length, 4, '4 players')
-//  ocat.file(res.players[0], { depth: 5 })
-  // inspect(res.players[0])
+  spok(t, res.players[0],
+    { $topic: 'player 0'
+    , seatno: 4
+    , chips: 33302
+    , chipsPreflop: 32852
+    , chipsFlop: 26893
+    , chipsTurn: 26893
+    , chipsRiver: 26893
+    , chipsShowdown: 26893
+    , chipsAfter: 26893
+    , m: 24
+    , preflop:
+      [ { type: 'call', ratio: 0.6, allin: false, amount: 3025, pot: 4825 }
+      , { type: 'call'
+        , ratio: 0.2
+        , allin: false
+        , amount: 2934
+        , pot: 14209 } ]
+    , flop: []
+    , turn: []
+    , river: []
+    , showdown: []
+    , sb: true
+    , preflopOrder: 2
+    , postflopOrder: 0
+    , pos: 'sb'
+    , cards: { card1: '7h', card2: '7d' }
+    , name: 'DmelloH'
+    , invested: true })
+  spok(t, res.players[1],
+    { $topic: 'player 1'
+    , seatno: 9
+    , chips: 6409
+    , chipsPreflop: 5559
+    , chipsFlop: 0
+    , chipsTurn: 0
+    , chipsRiver: 0
+    , chipsShowdown: 0
+    , chipsAfter: 16343
+    , m: 5
+    , preflop: [ { type: 'raise', ratio: 1.9, allin: true, amount: 5559, pot: 7850 } ]
+    , flop: []
+    , turn: []
+    , river: []
+    , showdown: [ { type: 'collect', ratio: 1, winall: true, amount: 16343 } ]
+    , hero: true
+    , cards: { card1: 'Qd', card2: 'Qs' }
+    , bb: true
+    , preflopOrder: 3
+    , postflopOrder: 1
+    , pos: 'bb'
+    , name: 'held'
+    , invested: true })
+  spok(t, res.players[2],
+    { $topic: 'player 2'
+    , seatno: 1
+    , chips: 3475
+    , chipsPreflop: 3425
+    , chipsFlop: 0
+    , chipsTurn: 0
+    , chipsRiver: 0
+    , chipsShowdown: 0
+    , chipsAfter: 0
+    , m: 2
+    , preflop: [ { type: 'raise', ratio: 4.3, allin: true, amount: 3425, pot: 1400 } ]
+    , flop: []
+    , turn: []
+    , river: []
+    , showdown: []
+    , preflopOrder: 0
+    , postflopOrder: 2
+    , pos: 'co'
+    , cards: { card1: '2c', card2: 'Ad' }
+    , name: 'Fischersito'
+    , invested: true })
+  spok(t, res.players[3],
+    { $topic: 'player 3'
+    , seatno: 3
+    , chips: 24314
+    , chipsPreflop: 24264
+    , chipsFlop: 24264
+    , chipsTurn: 24264
+    , chipsRiver: 24264
+    , chipsShowdown: 24264
+    , chipsAfter: 24264
+    , m: 17
+    , preflop: [ { type: 'fold', pot: 4825 } ]
+    , flop: []
+    , turn: []
+    , river: []
+    , showdown: []
+    , button: true
+    , preflopOrder: 1
+    , postflopOrder: 3
+    , pos: 'bu'
+    , name: 'Irisha2'
+    , invested: false })
   t.end()
 })
